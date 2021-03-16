0e9ced1278b3e58621d0261e72a5b4d2be246f01
Velii <45466195+Velii@users.noreply.github.com>
2021-03-16 18:36:06 +0100

    fruits updated

--- static/FVIClasses.ts
+++ static/FVIClasses.ts
@@ -18,21 +18,115 @@ const APPLE: FVIClass = {
   }
 }
 
-const BROCCOLI: FVIClass = {
-  title: 'Brokkoli',
+const APRICOT: FVIClass = {
+  title: 'Aprikose',
   thumbnail:
-    'https://images.unsplash.com/photo-1584270354949-c26b0d5b4a0c?w=1920&q=20',
+    'https://images.unsplash.com/photo-1586127145878-56a6b3dd537f?w=1920&q=20',
   information: [
-    'Hallo, Information #1',
-    'Und das ist Information #2',
-    'und die #3 gibts natürlich auch noch'
+    'Info1'
   ],
   nutrition: {
-    calories: '500 cal',
-    carbs: 'xy z',
-    fats: '0 g',
-    proteins: '14 g',
-    sugars: '1000 kg'
+    calories: '48 kcal',
+    carbs: '10 g',
+    fats: '0.1 g',
+    proteins: '0.9 g',
+    sugars: '7.7 g'
+  }
+}
+
+const AVOCADO: FVIClass = {
+  title: 'Avocado',
+  thumbnail: 
+  'https://images.unsplash.com/photo-1587825045005-c9cc5fa27203?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+  nutrition: {
+    calories: '160 kcal',
+    carbs: '0.4 g',
+    fats: '15 g',
+    proteins: '1.9 g',
+    sugars: '0.4 g'
+  }
+}
+
+const BANANA: FVIClass = {
+  title: 'Banane',
+  thumbnail:
+    'https://images.unsplash.com/photo-1528825871115-3581a5387919?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+  nutrition: {
+    calories: '93 kcal',
+    carbs: '20 g',
+    sugars: '17 g',
+    fats: '0.2 g',
+    proteins: '1 g'
+  }
+}
+
+const CANTALOUPE: FVIClass = {
+  title: 'Cantaloupe',
+  thumbnail:
+    'https://images.unsplash.com/photo-1598965869429-7ffa11f0b745?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+  nutrition: {
+    calories: '35 kcal',
+    carbs: '8 g',
+    fats: '0.2 g',
+    proteins: '0.8 g',
+    sugars: 'k.A.'
+  }
+}
+
+const CHERRY: FVIClass = {
+  title: 'Kirsche',
+  thumbnail:
+    'https://images.unsplash.com/photo-1528821099448-43ccebfbb899?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+  nutrition: {
+    calories: '64 kcal',
+    carbs: '13.3 g',
+    fats: '0.3 g',
+    proteins: '0.9 g',
+    sugars: '11.7 g'
+  }
+}
+
+const COCOS: FVIClass = {
+  title: 'Kokosnuss',
+  thumbnail:
+    'https://images.unsplash.com/photo-1589531786867-d06bad774195?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+  nutrition: {
+    calories: '354 kcal',
+    carbs: '15 g',
+    fats: '34 g',
+    proteins: '4 g',
+    sugars: '7 g'
+  }
+}
+
+const DATE: FVIClass = {
+  title: 'Datteln',
+  thumbnail:
+    'https://images.unsplash.com/photo-1598228961764-fdc1484f2af1?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+  nutrition: {
+    calories: '297 kcal',
+    carbs: '65 g',
+    fats: '0.5 g',
+    proteins: '2 g',
+    sugars: '65 g'
   }
 }
 
@@ -54,6 +148,38 @@ const GRAPE: FVIClass = {
   }
 }
 
+const GUAVA: FVIClass = {
+  title: 'Guave',
+  thumbnail:
+    'https://images.unsplash.com/photo-1600659090872-74ca5ca1023a?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+nutrition: {
+    calories: '61 kcal',
+    carbs: '12.89 g',
+    fats: '0.86 g',
+    proteins: '2.3 g',
+    sugars: '8.03 g'
+  }
+}
+
+const KIWI: FVIClass = {
+  title: 'Kiwi',
+  thumbnail:
+    'https://images.unsplash.com/photo-1591796079433-7f41b45eb95c?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+nutrition: {
+    calories: '62 kcal',
+    carbs: '9.1 g',
+    fats: '0.6 g',
+    proteins: '1 g',
+    sugars: '9 g'
+  }
+}
+
 const LEMON: FVIClass = {
   title: 'Zitrone',
   thumbnail:
@@ -69,6 +195,38 @@ const LEMON: FVIClass = {
     fats: '0.6 g',
     proteins: '0.7 g',
     sugars: '3 g'
+  }
+}
+
+const LIME: FVIClass = {
+  title: 'Limette',
+  thumbnail:
+    'https://images.unsplash.com/photo-1578855691621-8a08ea00d1fb?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+nutrition: {
+    calories: '48 kcal',
+    carbs: '1.9 g',
+    fats: '2.4 g',
+    proteins: '0.5 g',
+    sugars: '1.9 g'
+  }
+}
+
+const LYCHEE: FVIClass = {
+  title: 'Litschi',
+  thumbnail:
+    'https://images.unsplash.com/photo-1591600986686-bf5f872d2e4e?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+nutrition: {
+    calories: '76 kcal',
+    carbs: '17 g',
+    fats: '0.3 g',
+    proteins: '0.9 g',
+    sugars: '15.2 g'
   }
 }
 
@@ -90,6 +248,22 @@ const MANGO: FVIClass = {
   }
 }
 
+const MARACUJA: FVIClass = {
+  title: 'Maracuja',
+  thumbnail:
+    'https://images.unsplash.com/photo-1464287047163-e0b510b491e9?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+  nutrition: {
+    calories: '67 kcal',
+    carbs: '9.5 g',
+    fats: '0.4 g',
+    proteins: '2.4 g',
+    sugars: '9.5 g'
+  }
+}
+
 const ORANGE: FVIClass = {
   title: 'Orange',
   thumbnail:
@@ -105,6 +279,150 @@ const ORANGE: FVIClass = {
     sugars: '8.3 g',
     fats: '0.4 g',
     proteins: '1 g'
+  }
+}
+
+const PAPAYA: FVIClass = {
+  title: 'Papaya',
+  thumbnail:
+    'https://images.unsplash.com/photo-1526318472351-c75fcf070305?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+   nutrition: {
+    calories: '43 kcal',
+    carbs: '7.2 g',
+    fats: '0.3 g',
+    proteins: '0.5 g',
+    sugars: '7 g'
+  }
+}
+
+const PASSION_FRUIT: FVIClass = {
+  title: 'Passionsfrucht',
+  thumbnail:
+    'https://images.unsplash.com/photo-1604495772376-9657f0035eb5?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+  nutrition: {
+    calories: '67 kcal',
+    carbs: '9.5 g',
+    fats: '0.4 g',
+    proteins: '2.4 g',
+    sugars: '9.5 g'
+  }
+}
+
+const PEACH: FVIClass = {
+  title: 'Pfirsich',
+  thumbnail:
+    'https://images.unsplash.com/photo-1595124245030-41448b199d6d?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+    nutrition: {
+    calories: '39 kcal',
+    carbs: '8.9 g',
+    sugars: '8 g',
+    fats: '0.1 g',
+    proteins: '0.8 g'
+  }
+}
+
+const PEAR: FVIClass = {
+  title: 'Birne',
+  thumbnail:
+    'https://images.unsplash.com/photo-1601876819102-99560f772713?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+   nutrition: {
+      calories: '52 kcal',
+      carbs: '12.4 g',
+      fats: '0.3 g',
+      proteins: '0.5 g',
+      sugars: '10.2 g'
+    }
+}
+
+const PINEAPPLE: FVIClass = {
+  title: 'Ananas',
+  thumbnail:
+    'https://images.unsplash.com/photo-1550258987-190a2d41a8ba?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+  nutrition: {
+    calories: '57 kcal',
+    carbs: '12.4 g',
+    fats: '0.2 g',
+    proteins: '0.5 g',
+    sugars: '12.4 g'
+  }
+}
+
+const PLUM: FVIClass = {
+  title: 'Pflaume',
+  thumbnail:
+    'https://images.unsplash.com/photo-1603408209093-cd3c9af497d6?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+  nutrition: {
+    calories: '45 g',
+    carbs: '10 g',
+    fats: '0.2 g',
+    proteins: '0.6 g',
+    sugars: '10 g'
+  }
+}
+
+const POMEGRANATE: FVIClass = {
+  title: 'Granatapfel',
+  thumbnail:
+    'https://images.unsplash.com/photo-1597854710119-dbb43d562f78?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+  nutrition: {
+    calories: '80 kcal',
+    carbs: '19 g',
+    fats: '1.2 g',
+    proteins: '1.7 g',
+    sugars: '14 g'
+  }
+}
+
+const POMELO: FVIClass = {
+  title: 'Pampelmuse',
+  thumbnail:
+    'https://images.unsplash.com/photo-1550828553-7c8732c4bda6?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+  nutrition: {
+    calories: '80 kcal',
+    carbs: '19 g',
+    fats: '1.2 g',
+    proteins: '1.7 g',
+    sugars: '14 g'
+  }
+}
+
+const RASPBERRY: FVIClass = {
+  title: 'Himbeeren',
+  thumbnail:
+    'https://images.unsplash.com/photo-1569474998941-1665e838d492?w=1920&q=20',
+  information: [
+    'Info'
+  ],
+   nutrition: {
+    calories: '34 kcal',
+    carbs: '4.8 g',
+    fats: '0.3 g',
+    proteins: '1.3 g',
+    sugars: '4.6 g'
   }
 }
 
@@ -126,6 +444,25 @@ const STRAWBERRY: FVIClass = {
   }
 }
 
+const BROCCOLI: FVIClass = {
+  title: 'Brokoli',
+  thumbnail:
+    'https://images.unsplash.com/photo-1584270354949-c26b0d5b4a0c?w=1920&q=20',
+  information: [
+    'Hallo, Information #1',
+    'Und das ist Information #2',
+    'und die #3 gibts natürlich auch noch'
+  ],
+  nutrition: {
+    calories: '500 cal',
+    carbs: 'xy z',
+    fats: '0 g',
+    proteins: '14 g',
+    sugars: '1000 kg'
+  }
+}
+
+// To do -> die neuen Klassen am Ende hinzufügen
 const FVIClasses = [APPLE, BROCCOLI, GRAPE, LEMON, MANGO, ORANGE, STRAWBERRY]
 
 export default FVIClasses
