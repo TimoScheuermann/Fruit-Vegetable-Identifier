027bc1a3a5b3a646ccc08cc6b4d517250facf0af
Salocinf <nicolas.fuerhaupter@me.com>
2021-03-22 23:25:08 +0100

    added model v2 and model v3. model v3 is currently in service.

--- store/index.ts
+++ store/index.ts
@@ -46,8 +46,8 @@ export const mutations = {
     if (!state.model && !state.modelLoading) {
       state.modelLoading = true
 
-      const indexeddb = 'indexeddb://fvi-model-v1'
-      const modelPath = 'models/v1/model.json'
+      const indexeddb = 'indexeddb://fvi-model-v3'
+      const modelPath = 'models/v3/model.json'
 
       // try to load the model from local storage
       try {
