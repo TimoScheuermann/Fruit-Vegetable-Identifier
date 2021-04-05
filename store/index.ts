2d009fd4479ff8803df14b77296c347587bfa9ae
Salocinf <nicolas.fuerhaupter@me.com>
2021-04-05 11:46:06 +0200

    introduced models v5 through v8, model v8 is in use

--- store/index.ts
+++ store/index.ts
@@ -46,8 +46,8 @@ export const mutations = {
     if (!state.model && !state.modelLoading) {
       state.modelLoading = true
 
-      const indexeddb = 'indexeddb://fvi-model-v5'
-      const modelPath = 'models/v5/model.json'
+      const indexeddb = 'indexeddb://fvi-model-v8'
+      const modelPath = 'models/v8/model.json'
 
       // try to load the model from local storage
       try {
